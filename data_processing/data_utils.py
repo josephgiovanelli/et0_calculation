54464bcc635cb5a3b2bdd46f01805351377ad828
Joseph Giovanelli <j.giovanelli@unibo.it>
2023-02-23 12:14:53 +0100

    Apply formatter

--- data_processing/data_utils.py
+++ data_processing/data_utils.py
@@ -1,26 +1,130 @@
-#Headers
-VALUE_TYPES = [['ground_potential','GRND_WATER_G'],["meteo","WIND_GUST_MAX"],['meteo','SOLAR_RAD'],["meteo","BATTERY_TENS"],['meteo','PLUV_CURR'],
-                ['meteo','AIR_TEMP'],["ground_potential","DRIPPER"],["meteo","AIR_HUM"],["meteo","WIND_SPEED"],["ground_potential","BATTERY_TENS"],["meteo","ETC"],
-                ['meteo','WIND_DIRECTION'],["meteo","ET0"]]         
-VALUE_TYPES_INDEXES = ['wc','max_wind','solar_radiation','bat_tens','precipitation','air_temperature','irrigation','air_humidity','wind_speed','bt','etc','wind_dir','et0']
-USER_IN_PLANT = ['source','RefStructureId''refStructureName','companyName','fieldName','plantNum','plantRow','userId','wateringAdvice']
-VIEW_DATA_ORIGINAL = ["source","refStructureId","refStructureName","companyId","companyName","fieldId","fieldName","plantId","plantName",
-                        "plantNum","plantRow",'colture',"coltureType","nodeId","nodeDescription","detectedValueTypeId","detectedValueTypeDescription",
-                        'yy','xx','value','unit','date','time','latitude',"longitude","timestamp","zz"]
-TRANSCODING_FIELD = ['source','refStructureId','companyId','fieldId','plantId','nodeId','refStructureName','companyName','fieldName','plantNum',
-                       'plantRow','colture','coltureType','parcelCode','address','refNode','doProfile','xxProfile','yyProfile','zzProfile','sensorsNumber']
-HUMIDITY_BIN = ['timestamp','umidity_bin','count','refStructureName','companyName','fieldName','plantNum','plantRow','dumpId']
-INTERPOLATED = ['xx','yy','value','refStructureName','companyName','fieldName','plantNum','plantRow','timestamp','dumpId','zz']
+# Headers
+VALUE_TYPES = [
+    ["ground_potential", "GRND_WATER_G"],
+    ["meteo", "WIND_GUST_MAX"],
+    ["meteo", "SOLAR_RAD"],
+    ["meteo", "BATTERY_TENS"],
+    ["meteo", "PLUV_CURR"],
+    ["meteo", "AIR_TEMP"],
+    ["ground_potential", "DRIPPER"],
+    ["meteo", "AIR_HUM"],
+    ["meteo", "WIND_SPEED"],
+    ["ground_potential", "BATTERY_TENS"],
+    ["meteo", "ETC"],
+    ["meteo", "WIND_DIRECTION"],
+    ["meteo", "ET0"],
+]
+VALUE_TYPES_INDEXES = [
+    "wc",
+    "max_wind",
+    "solar_radiation",
+    "bat_tens",
+    "precipitation",
+    "air_temperature",
+    "irrigation",
+    "air_humidity",
+    "wind_speed",
+    "bt",
+    "etc",
+    "wind_dir",
+    "et0",
+]
+USER_IN_PLANT = [
+    "source",
+    "RefStructureId" "refStructureName",
+    "companyName",
+    "fieldName",
+    "plantNum",
+    "plantRow",
+    "userId",
+    "wateringAdvice",
+]
+VIEW_DATA_ORIGINAL = [
+    "source",
+    "refStructureId",
+    "refStructureName",
+    "companyId",
+    "companyName",
+    "fieldId",
+    "fieldName",
+    "plantId",
+    "plantName",
+    "plantNum",
+    "plantRow",
+    "colture",
+    "coltureType",
+    "nodeId",
+    "nodeDescription",
+    "detectedValueTypeId",
+    "detectedValueTypeDescription",
+    "yy",
+    "xx",
+    "value",
+    "unit",
+    "date",
+    "time",
+    "latitude",
+    "longitude",
+    "timestamp",
+    "zz",
+]
+TRANSCODING_FIELD = [
+    "source",
+    "refStructureId",
+    "companyId",
+    "fieldId",
+    "plantId",
+    "nodeId",
+    "refStructureName",
+    "companyName",
+    "fieldName",
+    "plantNum",
+    "plantRow",
+    "colture",
+    "coltureType",
+    "parcelCode",
+    "address",
+    "refNode",
+    "doProfile",
+    "xxProfile",
+    "yyProfile",
+    "zzProfile",
+    "sensorsNumber",
+]
+HUMIDITY_BIN = [
+    "timestamp",
+    "umidity_bin",
+    "count",
+    "refStructureName",
+    "companyName",
+    "fieldName",
+    "plantNum",
+    "plantRow",
+    "dumpId",
+]
+INTERPOLATED = [
+    "xx",
+    "yy",
+    "value",
+    "refStructureName",
+    "companyName",
+    "fieldName",
+    "plantNum",
+    "plantRow",
+    "timestamp",
+    "dumpId",
+    "zz",
+]
 
-#Simulation params
-HUM_BINS = [0,30,100,300,1500,10000]
+# Simulation params
+HUM_BINS = [0, 30, 100, 300, 1500, 10000]
 
-#Field params
+# Field params
 LATITUDE = 44.23484
 LONGITUDE = 11.8016815
 PLANT_NUM = 1
 PLANT_ROW = "T1 basso"
-SOURCE = 'iFarming'
+SOURCE = "iFarming"
 USER_ID = 11
 WATERING_ADVICE = False
 STRUCTURE_ID = "ZESPRI"
@@ -33,25 +137,28 @@ XXPROFILE = 100
 YY_PROFILE = 0
 ZZPROFILE = -60
 SENSORS_NUMBER = 12
-PLANT_NAME = ''
-NODE_DESCRIPTION = ''
-PARCEL_CODE = ''
-ADDRESS = ''
-REF_NODE = ''
+PLANT_NAME = ""
+NODE_DESCRIPTION = ""
+PARCEL_CODE = ""
+ADDRESS = ""
+REF_NODE = ""
 REF_STRUCTURE_NAME = "CIAO"
 COMPANY_NAME = "WITH"
 FIELD_NAME = "ET0 + DELTA"
 
-#uploader params
+# uploader params
 INITIAL_STATE = REF_STRUCTURE_NAME
 COST_MATRIX = COMPANY_NAME
 APPROACH = FIELD_NAME
 
-#Data granularity
-X = ['z20','z40','z60']
-Z = ['x0', 'x25', 'x50', 'x80']
+# Data granularity
+X = ["z20", "z40", "z60"]
+Z = ["x0", "x25", "x50", "x80"]
+
 
 def get_value_type(value_type):
     return VALUE_TYPES[VALUE_TYPES_INDEXES.index(value_type)][0]
+
+
 def get_value_type_id(value_type):
     return VALUE_TYPES[VALUE_TYPES_INDEXES.index(value_type)][1]
--- data_processing/main.py
+++ data_processing/main.py
@@ -1,6 +1,6 @@
 import db_uploader
 import data_utils as utils
-import os 
+import os
 from sshtunnel import SSHTunnelForwarder
 from psycopg2 import connect
 
@@ -9,46 +9,74 @@ SSH_PSW = "1234test*"
 
 
 DATA_PATH = "data"
-METEO_PATH =  os.path.join(DATA_PATH,'meteo')
-GROUND_PATH = os.path.join(DATA_PATH,"approaches",utils.COST_MATRIX.lower()+"costmatrix",utils.INITIAL_STATE.lower(),utils.APPROACH.lower())
+METEO_PATH = os.path.join(DATA_PATH, "meteo")
+GROUND_PATH = os.path.join(
+    DATA_PATH,
+    "approaches",
+    utils.COST_MATRIX.lower() + "costmatrix",
+    utils.INITIAL_STATE.lower(),
+    utils.APPROACH.lower(),
+)
 
-IRRIGATION_PATH = os.path.join(GROUND_PATH,"scheduledIrrigation.csv")
-WC_PATH = os.path.join(GROUND_PATH,"outputWaterContent.csv")
-WP_PATH = os.path.join(GROUND_PATH,"output.csv")
-METEO_FILES = ['air_humidity.csv','air_temperature.csv','precipitation.csv','solar_radiation.csv','wind_speed.csv']
+IRRIGATION_PATH = os.path.join(GROUND_PATH, "scheduledIrrigation.csv")
+WC_PATH = os.path.join(GROUND_PATH, "outputWaterContent.csv")
+WP_PATH = os.path.join(GROUND_PATH, "output.csv")
+METEO_FILES = [
+    "air_humidity.csv",
+    "air_temperature.csv",
+    "precipitation.csv",
+    "solar_radiation.csv",
+    "wind_speed.csv",
+]
+
 
 def setup_connection():
-  server = SSHTunnelForwarder(('isi-alfa.csr.unibo.it', 22),
-                              ssh_username= SSH_USERNAME,
-                              ssh_password= SSH_PSW,
-                              remote_bind_address=('137.204.74.52', 5432),
-                              local_bind_address=('127.0.0.1', 23349))
-  server.start()
-  print("SSH OK")
-  return connect(
-      database='smart_irrigation',
-      user='root',
-      host=server.local_bind_host,
-      port=server.local_bind_port,
-      password='smart_irrigation')
+    server = SSHTunnelForwarder(
+        ("isi-alfa.csr.unibo.it", 22),
+        ssh_username=SSH_USERNAME,
+        ssh_password=SSH_PSW,
+        remote_bind_address=("137.204.74.52", 5432),
+        local_bind_address=("127.0.0.1", 23349),
+    )
+    server.start()
+    print("SSH OK")
+    return connect(
+        database="smart_irrigation",
+        user="root",
+        host=server.local_bind_host,
+        port=server.local_bind_port,
+        password="smart_irrigation",
+    )
+
 
 conn = setup_connection()
 
-#upload user_in_plant // first three parms are lists
+# upload user_in_plant // first three parms are lists
 db_uploader.user_in_plant(conn)
-#upload transcoding_field // first three parms are lists
+# upload transcoding_field // first three parms are lists
 db_uploader.transcoding_field(conn)
-#upload wc
-db_uploader.sim_to_db(WC_PATH,utils.get_value_type('wc'),utils.get_value_type_id('wc'),conn)
-#upload irrigation
-db_uploader.sim_to_db(IRRIGATION_PATH,utils.get_value_type('irrigation'),utils.get_value_type_id('irrigation'),conn)
-#upload humidity_bin
-db_uploader.humidity_bin(WP_PATH,conn)
-#upload interpolated data
-db_uploader.load_interpolated_data(WP_PATH,conn)
+# upload wc
+db_uploader.sim_to_db(
+    WC_PATH, utils.get_value_type("wc"), utils.get_value_type_id("wc"), conn
+)
+# upload irrigation
+db_uploader.sim_to_db(
+    IRRIGATION_PATH,
+    utils.get_value_type("irrigation"),
+    utils.get_value_type_id("irrigation"),
+    conn,
+)
+# upload humidity_bin
+db_uploader.humidity_bin(WP_PATH, conn)
+# upload interpolated data
+db_uploader.load_interpolated_data(WP_PATH, conn)
 
-#upload meteo
-for i in range (0,len(METEO_FILES)):
-    db_uploader.sim_to_db(os.path.join(METEO_PATH,METEO_FILES[i]), utils.get_value_type(METEO_FILES[i].split('.')[0]),utils.get_value_type_id(METEO_FILES[i].split('.')[0]),conn)
+# upload meteo
+for i in range(0, len(METEO_FILES)):
+    db_uploader.sim_to_db(
+        os.path.join(METEO_PATH, METEO_FILES[i]),
+        utils.get_value_type(METEO_FILES[i].split(".")[0]),
+        utils.get_value_type_id(METEO_FILES[i].split(".")[0]),
+        conn,
+    )
 conn.close()
-
